f805c77df789cffc02a2412fcf094824e71ce4ee
Jared Hanson <jaredhanson@gmail.com>
2022-03-16 14:16:25 -0700

    Initial commit.

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var passport = require('passport');
+var BearerStrategy = require('passport-http-bearer');
+var db = require('../db');
+
+passport.use(new BearerStrategy(function(token, cb) {
+  // TODO: verify the token
+  return cb(null, { id: 1 });
+}));
+
+var router = express.Router();
+
+router.post('/', passport.authenticate('bearer', { session: false }), function(req, res, next) {
+  req.body.title = req.body.title.trim();
+  next();
+}, function(req, res, next) {
+  if (req.body.title !== '') { return next(); }
+  // TODO: error handling
+  return res.redirect('/' + (req.body.filter || ''));
+}, function(req, res, next) {
+  db.run('INSERT INTO todos (owner_id, title, completed) VALUES (?, ?, ?)', [
+    req.user.id,
+    req.body.title,
+    req.body.completed == true ? 1 : null
+  ], function(err) {
+    if (err) { return next(err); }
+    res.json({ title: req.body.title });
+  });
+});
+
+module.exports = router;
